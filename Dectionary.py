d8742c17b7f411cd583fc6e510fcfdbf4aa87c11
djamel1 <benlaiterdjamel@gmail.com>
2018-10-03 18:39:57 +0200

    add new file

---
+++ Dectionary.py
@@ -0,0 +1,11 @@
+def main():
+    Student={'name':"benlaiter djamel",'Age':25,'Salire':0}#name.... is Key
+    Student['name']="moumen"
+    Student['Domaine']="informatique"
+    print(Student)
+    del Student['name']
+    print(Student['Age'])
+    print(Student,type(Student))
+    Student.clear()
+    print(Student)
+if __name__ == '__main__':main()
